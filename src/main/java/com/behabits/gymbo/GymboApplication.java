d54f9c91b496043ecc0b738bf1591bae61824e32
polfc21 <polfarreny@gmail.com>
2023-02-14 23:39:26 +0100

    Initial Gymbo

---
+++ src/main/java/com/behabits/gymbo/GymboApplication.java
@@ -0,0 +1,13 @@
+package com.behabits.gymbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/behabits/gymbo/GymboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behabits.gymbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
