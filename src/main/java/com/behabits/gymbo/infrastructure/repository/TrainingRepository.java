cb45004a19a6b4cddd35a0b1d1bf650ae686d02b
polfc21 <polfarreny@gmail.com>
2023-02-24 20:46:47 +0100

    fixed query in TrainingRepository #14

--- src/main/java/com/behabits/gymbo/infrastructure/repository/TrainingRepository.java
+++ src/main/java/com/behabits/gymbo/infrastructure/repository/TrainingRepository.java
@@ -2,12 +2,15 @@ package com.behabits.gymbo.infrastructure.repository;
 
 import com.behabits.gymbo.infrastructure.repository.entity.TrainingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.List;
 
 @Repository
 public interface TrainingRepository extends JpaRepository<TrainingEntity, Long> {
-    List<TrainingEntity> findAllByDate_Month(Month month);
+    @Query(value = "SELECT COUNT(*) FROM training WHERE EXTRACT(MONTH FROM training_date) = ?1", nativeQuery = true)
+    List<TrainingEntity> findAllByMonth(Month month);
 }
