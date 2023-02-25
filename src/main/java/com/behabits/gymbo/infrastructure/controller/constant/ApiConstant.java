accd7c00caf8d77209821ec00e020941f3155d88
polfc21 <polfarreny@gmail.com>
2023-02-25 14:31:22 +0100

    added constant endpoints class #19

---
+++ src/main/java/com/behabits/gymbo/infrastructure/controller/constant/ApiConstant.java
@@ -0,0 +1,10 @@
+package com.behabits.gymbo.infrastructure.controller.constant;
+
+public class ApiConstant {
+    public static final String API = "/api";
+    public static final String API_V1 = API + "/v1";
+    public static final String ID = "/{id}";
+    public static final String TRAININGS = "/trainings";
+    public static final String EXERCISES = "/exercises";
+    public static final String SERIES = "/series";
+}
