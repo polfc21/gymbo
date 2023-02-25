36616f16c74b551d2dc1762342d23fa7b84cd980
polfc21 <polfarreny@gmail.com>
2023-02-25 14:01:36 +0100

    refactor code smells

--- src/main/java/com/behabits/gymbo/infrastructure/repository/mapper/SerieEntityMapper.java
+++ src/main/java/com/behabits/gymbo/infrastructure/repository/mapper/SerieEntityMapper.java
@@ -5,7 +5,6 @@ import com.behabits.gymbo.infrastructure.repository.entity.SerieEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class SerieEntityMapper {
