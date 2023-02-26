817fd2e945e536cc1072bb17185195fc2ff47470
polfc21 <polfarreny@gmail.com>
2023-02-26 19:41:52 +0100

    added Training and Exercise dao implementation tests #27

---
+++ src/test/java/com/behabits/gymbo/infrastructure/dao/JpaExerciseDaoTest.java
@@ -0,0 +1,80 @@
+package com.behabits.gymbo.infrastructure.dao;
+
+import com.behabits.gymbo.domain.models.Exercise;
+import com.behabits.gymbo.domain.repositories.ExerciseModelRepository;
+import com.behabits.gymbo.infrastructure.repository.ExerciseRepository;
+import com.behabits.gymbo.infrastructure.repository.entity.ExerciseEntity;
+import com.behabits.gymbo.infrastructure.repository.mapper.ExerciseEntityMapper;
+import com.behabits.gymbo.infrastructure.repository.repositories.ExerciseEntityRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class JpaExerciseDaoTest {
+
+    @InjectMocks
+    private JpaExerciseDao exerciseDao;
+
+    @Mock
+    private ExerciseRepository exerciseRepository;
+
+    @Mock
+    private ExerciseEntityMapper mapper;
+
+    private final ExerciseEntityRepository exerciseEntityRepository = new ExerciseEntityRepository();
+
+    private final ExerciseModelRepository exerciseModelRepository = new ExerciseModelRepository();
+
+    @Test
+    void givenSquatExerciseWhenCreateExerciseThenReturnSquatExercise() {
+        Exercise squatExercise = this.exerciseModelRepository.buildSquatExercise();
+        ExerciseEntity squatExerciseEntity = this.exerciseEntityRepository.getSquatExercise();
+
+        when(this.mapper.toEntity(squatExercise)).thenReturn(squatExerciseEntity);
+        when(this.exerciseRepository.save(squatExerciseEntity)).thenReturn(squatExerciseEntity);
+        when(this.mapper.toDomain(squatExerciseEntity)).thenReturn(squatExercise);
+
+        assertThat(this.exerciseDao.createExercise(squatExercise), is(squatExercise));
+    }
+
+    @Test
+    void givenSquatExerciseWithSeriesWhenCreateExerciseThenReturnSquatExerciseWithSeries() {
+        Exercise squatExercise = this.exerciseModelRepository.buildSquatExerciseWithSquatSeries();
+        ExerciseEntity squatExerciseEntity = this.exerciseEntityRepository.getSquatExerciseWithSeries();
+
+        when(this.mapper.toEntity(squatExercise)).thenReturn(squatExerciseEntity);
+        when(this.exerciseRepository.save(squatExerciseEntity)).thenReturn(squatExerciseEntity);
+        when(this.mapper.toDomain(squatExerciseEntity)).thenReturn(squatExercise);
+
+        assertThat(this.exerciseDao.createExercise(squatExercise), is(squatExercise));
+    }
+
+    @Test
+    void givenExistentIdWhenFindExerciseByIdThenReturnExercise() {
+        Exercise squatExercise = this.exerciseModelRepository.buildSquatExerciseWithSquatSeries();
+        ExerciseEntity squatExerciseEntity = this.exerciseEntityRepository.getSquatExerciseWithSeries();
+
+        when(this.exerciseRepository.findById(squatExerciseEntity.getId())).thenReturn(Optional.of(squatExerciseEntity));
+        when(this.mapper.toDomain(squatExerciseEntity)).thenReturn(squatExercise);
+
+        assertThat(this.exerciseDao.findExerciseById(squatExercise.getId()), is(squatExercise));
+    }
+
+    @Test
+    void givenNonExistentIdWhenFindExerciseByIdThenReturnNull() {
+        when(this.exerciseRepository.findById(1L)).thenReturn(Optional.empty());
+        when(this.mapper.toDomain((ExerciseEntity) null)).thenReturn(null);
+
+        assertNull(this.exerciseDao.findExerciseById(1L));
+    }
+}
---
+++ src/test/java/com/behabits/gymbo/infrastructure/dao/JpaTrainingDaoTest.java
@@ -0,0 +1,95 @@
+package com.behabits.gymbo.infrastructure.dao;
+
+import com.behabits.gymbo.domain.models.Training;
+import com.behabits.gymbo.domain.repositories.TrainingModelRepository;
+import com.behabits.gymbo.infrastructure.repository.TrainingRepository;
+import com.behabits.gymbo.infrastructure.repository.entity.TrainingEntity;
+import com.behabits.gymbo.infrastructure.repository.mapper.TrainingEntityMapper;
+import com.behabits.gymbo.infrastructure.repository.repositories.TrainingEntityRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class JpaTrainingDaoTest {
+
+    @InjectMocks
+    private JpaTrainingDao trainingDao;
+
+    @Mock
+    private TrainingRepository trainingRepository;
+
+    @Mock
+    private TrainingEntityMapper mapper;
+
+    private final TrainingEntityRepository trainingEntityRepository = new TrainingEntityRepository();
+
+    private final TrainingModelRepository trainingModelRepository = new TrainingModelRepository();
+
+    @Test
+    void givenLegTrainingWhenCreateTrainingThenReturnTraining() {
+        Training legTraining = this.trainingModelRepository.buildLegTraining();
+        TrainingEntity legTrainingEntity = this.trainingEntityRepository.getLegTraining();
+
+        when(this.mapper.toEntity(legTraining)).thenReturn(legTrainingEntity);
+        when(this.trainingRepository.save(legTrainingEntity)).thenReturn(legTrainingEntity);
+        when(this.mapper.toDomain(legTrainingEntity)).thenReturn(legTraining);
+
+        assertThat(this.trainingDao.createTraining(legTraining), is(legTraining));
+    }
+
+    @Test
+    void givenLegTrainingWithSquatExerciseWhenCreateTrainingThenReturnTraining() {
+        Training legTraining = this.trainingModelRepository.buildLegTrainingWithSquatExercise();
+        TrainingEntity legTrainingEntity = this.trainingEntityRepository.getLegTrainingWithSquatExerciseWithSeries();
+
+        when(this.mapper.toEntity(legTraining)).thenReturn(legTrainingEntity);
+        when(this.trainingRepository.save(legTrainingEntity)).thenReturn(legTrainingEntity);
+        when(this.mapper.toDomain(legTrainingEntity)).thenReturn(legTraining);
+
+        assertThat(this.trainingDao.createTraining(legTraining), is(legTraining));
+    }
+
+    @Test
+    void givenMonthWhenFindTrainingsByMonthThenReturnTrainings() {
+        Training legTraining = this.trainingModelRepository.buildLegTraining();
+        TrainingEntity legTrainingEntity = this.trainingEntityRepository.getLegTraining();
+        Month legTrainingMonth = legTraining.getTrainingDate().getMonth();
+        Month legTrainingEntityMonth = legTrainingEntity.getTrainingDate().getMonth();
+
+        when(this.trainingRepository.findAllByMonth(legTrainingEntityMonth)).thenReturn(List.of(legTrainingEntity));
+        when(this.mapper.toDomain(legTrainingEntity)).thenReturn(legTraining);
+
+        assertThat(this.trainingDao.findTrainingsByMonth(legTrainingMonth), is(List.of(legTraining)));
+    }
+
+    @Test
+    void givenExistentIdWhenFindTrainingByIdThenReturnTraining() {
+        Training legTraining = this.trainingModelRepository.buildLegTraining();
+        TrainingEntity legTrainingEntity = this.trainingEntityRepository.getLegTraining();
+
+        when(this.trainingRepository.findById(legTrainingEntity.getId())).thenReturn(Optional.of(legTrainingEntity));
+        when(this.mapper.toDomain(legTrainingEntity)).thenReturn(legTraining);
+
+        assertThat(this.trainingDao.findTrainingById(legTraining.getId()), is(legTraining));
+    }
+
+    @Test
+    void givenNoExistentIdWhenFindTrainingByIdThenReturnNull() {
+        when(this.trainingRepository.findById(1L)).thenReturn(Optional.empty());
+        when(this.mapper.toDomain(null)).thenReturn(null);
+
+        assertNull(this.trainingDao.findTrainingById(1L));
+    }
+}
