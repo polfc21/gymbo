44dd1dff8fad46060fd3fa692b31fb0345794a80
polfarrenycapdevila <pfarreny@nnergix.com>
2023-05-04 22:34:04 +0200

    added UserEntityMapper #53

---
+++ src/main/java/com/behabits/gymbo/infrastructure/repository/mapper/UserEntityMapper.java
@@ -0,0 +1,36 @@
+package com.behabits.gymbo.infrastructure.repository.mapper;
+
+import com.behabits.gymbo.domain.models.User;
+import com.behabits.gymbo.infrastructure.repository.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserEntityMapper {
+
+    public User toDomain(UserEntity entity) {
+        if (entity == null) {
+            return null;
+        }
+        User domain = new User();
+        domain.setId(entity.getId());
+        domain.setUsername(entity.getUsername());
+        domain.setFirstName(entity.getFirstName());
+        domain.setLastName(entity.getLastName());
+        domain.setEmail(entity.getEmail());
+        domain.setPassword(entity.getPassword());
+        return domain;
+    }
+
+    public UserEntity toEntity(User domain) {
+        UserEntity entity = new UserEntity();
+        entity.setId(domain.getId());
+        entity.setUsername(domain.getUsername());
+        entity.setFirstName(domain.getFirstName());
+        entity.setLastName(domain.getLastName());
+        entity.setEmail(domain.getEmail());
+        entity.setPassword(domain.getPassword());
+        return entity;
+    }
+}
---
+++ src/test/java/com/behabits/gymbo/infrastructure/repository/mapper/UserEntityMapperTest.java
@@ -0,0 +1,51 @@
+package com.behabits.gymbo.infrastructure.repository.mapper;
+
+import com.behabits.gymbo.domain.models.User;
+import com.behabits.gymbo.domain.repositories.UserModelRepository;
+import com.behabits.gymbo.infrastructure.repository.entity.UserEntity;
+import com.behabits.gymbo.infrastructure.repository.repositories.UserEntityRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest
+public class UserEntityMapperTest {
+
+    @Autowired
+    private UserEntityMapper mapper;
+
+    private final UserEntityRepository userEntityRepository = new UserEntityRepository();
+
+    private final UserModelRepository userModelRepository = new UserModelRepository();
+
+    @Test
+    void givenUserWhenMapToEntityThenReturnUserEntity() {
+        User user = this.userModelRepository.getUser();
+
+        UserEntity userEntity = this.mapper.toEntity(user);
+
+        assertThat(userEntity.getId(), is(user.getId()));
+        assertThat(userEntity.getUsername(), is(user.getUsername()));
+        assertThat(userEntity.getPassword(), is(user.getPassword()));
+        assertThat(userEntity.getFirstName(), is(user.getFirstName()));
+        assertThat(userEntity.getLastName(), is(user.getLastName()));
+        assertThat(userEntity.getEmail(), is(user.getEmail()));
+    }
+
+    @Test
+    void givenUserEntityWhenMapToDomainThenReturnUser() {
+        UserEntity userEntity = this.userEntityRepository.getUser();
+
+        User user = this.mapper.toDomain(userEntity);
+
+        assertThat(user.getId(), is(userEntity.getId()));
+        assertThat(user.getUsername(), is(userEntity.getUsername()));
+        assertThat(user.getPassword(), is(userEntity.getPassword()));
+        assertThat(user.getFirstName(), is(userEntity.getFirstName()));
+        assertThat(user.getLastName(), is(userEntity.getLastName()));
+        assertThat(user.getEmail(), is(userEntity.getEmail()));
+    }
+}
