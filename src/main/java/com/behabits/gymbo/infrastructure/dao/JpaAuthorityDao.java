3007a3e6da6691ab795c3082af0526d641558d30
polfc21 <polfarreny@gmail.com>
2023-05-11 01:04:47 +0200

    added check permissions of exercises in AuthorityDao #56

--- src/main/java/com/behabits/gymbo/infrastructure/dao/JpaAuthorityDao.java
+++ src/main/java/com/behabits/gymbo/infrastructure/dao/JpaAuthorityDao.java
@@ -2,10 +2,13 @@ package com.behabits.gymbo.infrastructure.dao;
 
 import com.behabits.gymbo.domain.daos.AuthorityDao;
 import com.behabits.gymbo.domain.exceptions.PermissionsException;
+import com.behabits.gymbo.domain.models.Exercise;
 import com.behabits.gymbo.domain.models.Training;
 import com.behabits.gymbo.domain.models.User;
+import com.behabits.gymbo.infrastructure.repository.ExerciseRepository;
 import com.behabits.gymbo.infrastructure.repository.TrainingRepository;
 import com.behabits.gymbo.infrastructure.repository.UserRepository;
+import com.behabits.gymbo.infrastructure.repository.entity.ExerciseEntity;
 import com.behabits.gymbo.infrastructure.repository.entity.TrainingEntity;
 import com.behabits.gymbo.infrastructure.repository.entity.UserEntity;
 import com.behabits.gymbo.infrastructure.repository.mapper.UserEntityMapper;
@@ -18,9 +21,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class JpaAuthorityDao implements AuthorityDao {
 
+    private static final String USER_HAS_NOT_PERMISSIONS = "User doesn't have permissions";
+
     private final UserEntityMapper userMapper;
     private final UserRepository userRepository;
     private final TrainingRepository trainingRepository;
+    private final ExerciseRepository exerciseRepository;
 
     @Override
     public User getLoggedUser() {
@@ -34,7 +40,16 @@ public class JpaAuthorityDao implements AuthorityDao {
         Long loggedUserId = this.getLoggedUser().getId();
         TrainingEntity trainingToCheck = this.trainingRepository.findByIdAndPlayerId(training.getId(), loggedUserId);
         if (trainingToCheck == null) {
-            throw new PermissionsException("User doesn't have permissions to this training");
+            throw new PermissionsException(USER_HAS_NOT_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void checkLoggedUserHasPermissions(Exercise exercise) {
+        Long loggedUserId = this.getLoggedUser().getId();
+        ExerciseEntity exerciseToCheck = this.exerciseRepository.findByIdAndPlayerId(exercise.getId(), loggedUserId);
+        if (exerciseToCheck == null) {
+            throw new PermissionsException(USER_HAS_NOT_PERMISSIONS);
         }
     }
 
--- src/test/java/com/behabits/gymbo/infrastructure/dao/JpaAuthorityDaoTest.java
+++ src/test/java/com/behabits/gymbo/infrastructure/dao/JpaAuthorityDaoTest.java
@@ -1,12 +1,16 @@
 package com.behabits.gymbo.infrastructure.dao;
 
 import com.behabits.gymbo.domain.exceptions.PermissionsException;
+import com.behabits.gymbo.domain.models.Exercise;
 import com.behabits.gymbo.domain.models.Training;
 import com.behabits.gymbo.domain.models.User;
+import com.behabits.gymbo.domain.repositories.ExerciseModelRepository;
 import com.behabits.gymbo.domain.repositories.TrainingModelRepository;
 import com.behabits.gymbo.domain.repositories.UserModelRepository;
+import com.behabits.gymbo.infrastructure.repository.ExerciseRepository;
 import com.behabits.gymbo.infrastructure.repository.TrainingRepository;
 import com.behabits.gymbo.infrastructure.repository.UserRepository;
+import com.behabits.gymbo.infrastructure.repository.entity.ExerciseEntity;
 import com.behabits.gymbo.infrastructure.repository.entity.TrainingEntity;
 import com.behabits.gymbo.infrastructure.repository.entity.UserEntity;
 import com.behabits.gymbo.infrastructure.repository.mapper.UserEntityMapper;
@@ -39,6 +43,9 @@ class JpaAuthorityDaoTest {
     private TrainingRepository trainingRepository;
 
     @Mock
+    private ExerciseRepository exerciseRepository;
+
+    @Mock
     private UserEntityMapper userMapper;
 
     private final UserEntityRepository userEntityRepository = new UserEntityRepository();
@@ -46,6 +53,7 @@ class JpaAuthorityDaoTest {
     private final UserModelRepository userModelRepository = new UserModelRepository();
 
     private final TrainingModelRepository trainingModelRepository = new TrainingModelRepository();
+    private final ExerciseModelRepository exerciseModelRepository = new ExerciseModelRepository();
 
     @Test
     void givenLoggedUserWhenGetLoggedUserThenReturnLoggedUser() {
@@ -92,5 +100,39 @@ class JpaAuthorityDaoTest {
         when(this.trainingRepository.findByIdAndPlayerId(training.getId(), user.getId())).thenReturn(null);
 
         assertThrows(PermissionsException.class, () -> this.authorityDao.checkLoggedUserHasPermissions(training));
+    }
+
+    @Test
+    void givenLoggedUserHasPermissionsWhenCheckHasExercisePermissionsThenDoNothing() {
+        Exercise exercise = this.exerciseModelRepository.getSquatExercise();
+        UserEntity loggedUser = this.userEntityRepository.getUser();
+        User user = this.userModelRepository.getUser();
+        Authentication authentication = new TestingAuthenticationToken(loggedUser.getUsername(), "password");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        when(this.userRepository.findByUsername(loggedUser.getUsername())).thenReturn(loggedUser);
+        when(this.userMapper.toDomain(loggedUser)).thenReturn(user);
+        when(this.exerciseRepository.findByIdAndPlayerId(exercise.getId(), user.getId())).thenReturn(new ExerciseEntity());
+
+        try {
+            this.authorityDao.checkLoggedUserHasPermissions(exercise);
+        } catch (Exception e) {
+            fail("Should not throw exception");
+        }
+    }
+
+    @Test
+    void givenLoggedUserHasNotPermissionsWhenCheckHasExercisePermissionsThenPermissionsException() {
+        Exercise exercise = this.exerciseModelRepository.getSquatExercise();
+        UserEntity loggedUser = this.userEntityRepository.getUser();
+        User user = this.userModelRepository.getUser();
+        Authentication authentication = new TestingAuthenticationToken(loggedUser.getUsername(), "password");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        when(this.userRepository.findByUsername(loggedUser.getUsername())).thenReturn(loggedUser);
+        when(this.userMapper.toDomain(loggedUser)).thenReturn(user);
+        when(this.exerciseRepository.findByIdAndPlayerId(exercise.getId(), user.getId())).thenReturn(null);
+
+        assertThrows(PermissionsException.class, () -> this.authorityDao.checkLoggedUserHasPermissions(exercise));
     }
 }
