327f34983cce84ba0a0999544ffd420bc072dfc3
polfc21 <polfarreny@gmail.com>
2023-06-21 15:03:36 +0200

    modified UserDetailsService responsibilities #71

--- src/main/java/com/behabits/gymbo/domain/services/UserService.java
+++ src/main/java/com/behabits/gymbo/domain/services/UserService.java
@@ -1,7 +1,10 @@
 package com.behabits.gymbo.domain.services;
 
 import com.behabits.gymbo.domain.models.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
+    UserDetails loadUserByUsername(String username);
     User createUser(User user);
 }
