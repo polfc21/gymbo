80c2c33d6b48cf77ceadf7204ff725f646c614e8
polfc21 <polfarreny@gmail.com>
2023-06-30 01:03:14 +0200

    added context loads test #78

--- src/test/java/com/behabits/gymbo/GymboApplicationTests.java
+++ src/test/java/com/behabits/gymbo/GymboApplicationTests.java
@@ -3,11 +3,14 @@ package com.behabits.gymbo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @SpringBootTest
 class GymboApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat("Context loads", true);
     }
 
 }
