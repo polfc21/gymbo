4461eb3d88d435fc9583cbbffb72a18bf9fc5fe7
polfc21 <polfarreny@gmail.com>
2023-06-29 20:43:03 +0200

    deleted JwtService #76

--- src/main/java/com/behabits/gymbo/domain/services/JwtService.java
+++
@@ -1,8 +0,0 @@
-package com.behabits.gymbo.domain.services;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
-public interface JwtService {
-    Boolean isValid(String token);
-    UsernamePasswordAuthenticationToken getAuthentication(String token);
-}
