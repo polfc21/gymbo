7e5df3fb6fd0d7fcce437e35966fbdfd1383638b
polfc21 <polfarreny@gmail.com>
2023-09-01 00:29:57 +0200

    added Exercise mapping in LinkApiMapper #97

--- src/test/java/com/behabits/gymbo/infrastructure/controller/mapper/LinkApiMapperTest.java
+++ src/test/java/com/behabits/gymbo/infrastructure/controller/mapper/LinkApiMapperTest.java
@@ -5,6 +5,7 @@ import com.behabits.gymbo.domain.repositories.LinkModelRepository;
 import com.behabits.gymbo.infrastructure.controller.dto.request.LinkRequest;
 import com.behabits.gymbo.infrastructure.controller.dto.response.LinkResponse;
 import com.behabits.gymbo.infrastructure.controller.repositories.request.LinkRequestRepository;
+import com.behabits.gymbo.infrastructure.controller.repositories.response.LinkResponseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,9 +19,12 @@ class LinkApiMapperTest {
     @Autowired
     private LinkApiMapper linkApiMapper;
 
+    private final LinkModelRepository linkModelRepository = new LinkModelRepository();
+    private final LinkRequestRepository linkRequestRepository = new LinkRequestRepository();
+
     @Test
     void givenLinkRequestWhenMapToDomainThenReturnLink() {
-        LinkRequest linkRequest = new LinkRequestRepository().getCorrectLinkRequest();
+        LinkRequest linkRequest = this.linkRequestRepository.getCorrectLinkRequest();
 
         Link link = this.linkApiMapper.toDomain(linkRequest);
 
@@ -29,12 +33,33 @@ class LinkApiMapperTest {
 
     @Test
     void givenLinkWhenMapToResponseThenReturnLinkResponse() {
-        Link link = new LinkModelRepository().getLink();
+        Link link = this.linkModelRepository.getLink();
 
         LinkResponse linkResponse = this.linkApiMapper.toResponse(link);
 
         assertThat(linkResponse.getId(), is(link.getId()));
         assertThat(linkResponse.getEntity(), is(link.getEntity()));
+    }
+
+    @Test
+    void givenLinkRequestWithExerciseWhenMapToDomainThenReturnLinkWithExercise() {
+        LinkRequest linkRequest = this.linkRequestRepository.getLinkWithExerciseRequest();
+
+        Link link = this.linkApiMapper.toDomain(linkRequest);
+
+        assertThat(link.getEntity(), is(linkRequest.getEntity()));
+        assertThat(link.getExercise().getId(), is(linkRequest.getExerciseId()));
+    }
+
+    @Test
+    void givenLinkWithExerciseWhenMapToResponseThenReturnLinkResponseWithExercise() {
+        Link link = this.linkModelRepository.getLinkWithExercise();
+
+        LinkResponse linkResponse = this.linkApiMapper.toResponse(link);
+
+        assertThat(linkResponse.getId(), is(link.getId()));
+        assertThat(linkResponse.getEntity(), is(link.getEntity()));
+        assertThat(linkResponse.getExercise().getId(), is(link.getExercise().getId()));
     }
 
 }
