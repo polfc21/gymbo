a29f2a380771b00579b2bab94290b30b88de31d9
polfc21 <polfarreny@gmail.com>
2023-09-03 21:11:42 +0200

    added findAllByReviewedId query #100

--- src/main/java/com/behabits/gymbo/infrastructure/repository/ReviewRepository.java
+++ src/main/java/com/behabits/gymbo/infrastructure/repository/ReviewRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
     @Query(value = "SELECT r.* FROM review r WHERE r.id = ?1 AND (r.reviewer_id = ?2 OR r.reviewed_id = ?2)", nativeQuery = true)
     ReviewEntity findByIdAndPlayerIsReviewerOrReviewed(Long id, Long playerId);
+    List<ReviewEntity> findAllByReviewedId(Long reviewedId);
 }
