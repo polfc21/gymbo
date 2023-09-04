10833ea93d69964e8dc07f38660a9c014238b836
polfc21 <polfarreny@gmail.com>
2023-09-05 00:26:58 +0200

    added findUsersInKilometersOrderedByDistanceFromPlayerId query #102

--- src/main/java/com/behabits/gymbo/infrastructure/repository/UserRepository.java
+++ src/main/java/com/behabits/gymbo/infrastructure/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.behabits.gymbo.infrastructure.repository;
 
 import com.behabits.gymbo.infrastructure.repository.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUsername(String username);
+    @Query(value = "SELECT p.* FROM player p JOIN location l ON p.id = l.player_id WHERE ST_DWithin(ST_Transform(l.geometry, 32631), ST_Transform((SELECT l.geometry FROM location l WHERE l.player_id = ?1), 32631), ?2 * 1000) AND l.player_id != ?1 ORDER BY ST_Distance(geometry, (SELECT l.geometry FROM location l WHERE l.player_id = ?1))", nativeQuery = true)
+    List<UserEntity> findUsersInKilometersOrderedByDistanceFromPlayerId(Long playerId, Double kilometers);
 }
