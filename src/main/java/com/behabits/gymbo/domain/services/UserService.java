125546de2453bdaa9c2b0249778d823f1f72e252
polfc21 <polfarreny@gmail.com>
2023-09-05 00:28:04 +0200

    added findUsersInKilometersOrderedByDistanceFromLoggedUser in UserService #102

--- src/main/java/com/behabits/gymbo/domain/services/UserService.java
+++ src/main/java/com/behabits/gymbo/domain/services/UserService.java
@@ -4,8 +4,11 @@ import com.behabits.gymbo.domain.models.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.List;
+
 public interface UserService extends UserDetailsService {
     UserDetails loadUserByUsername(String username);
     User findUserByUsername(String username);
     User createUser(User user);
+    List<User> findUsersInKilometersOrderedByDistanceFromLoggedUser(Double kilometers);
 }
