6f19d3d855ab0e5e6bbece27b247794b92a212d0
polfc21 <polfarreny@gmail.com>
2023-09-08 00:23:30 +0200

    added LinkValidator #98

---
+++ src/main/java/com/behabits/gymbo/infrastructure/controller/dto/validator/LinkValidator.java
@@ -0,0 +1,17 @@
+package com.behabits.gymbo.infrastructure.controller.dto.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LinkValidator implements ConstraintValidator<ValidLink, String> {
+
+    @Override
+    public boolean isValid(String request, ConstraintValidatorContext constraintValidatorContext) {
+        return this.isValidEntity(request);
+    }
+
+    private boolean isValidEntity(String entity) {
+        return entity.equals("EXERCISE") || entity.equals("USER");
+    }
+
+}
